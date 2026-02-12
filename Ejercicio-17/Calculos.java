7d98fd760ad084562c1882955b1da62f7902f33f
giraldoandrea <angiraldore@unal.edu.co>
2026-02-12 11:12:54 -0500

    Create Calculos.java

---
+++ Ejercicio-17/Calculos.java
@@ -0,0 +1,11 @@
+public class Calculos {
+    
+public static double calcular_longitud_circuferencia(double radio) {
+   return 2 * Math.PI * radio;
+}
+
+public static double calcular_area_circulo(double radio) {
+   return Math.PI * Math.pow(radio,2);
+  }
+    
+}
