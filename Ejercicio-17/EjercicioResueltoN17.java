c4462eca938b2077ad3415b270a743005834a9bc
giraldoandrea <angiraldore@unal.edu.co>
2026-02-12 11:11:50 -0500

    Create EjercicioResueltoN17.java

---
+++ Ejercicio-17/EjercicioResueltoN17.java
@@ -0,0 +1,17 @@
+public class EjercicioResueltoN17 {
+
+
+    public static void main(String[] args) {
+    double radio = 3;
+    double cuadrado, cubo;
+
+
+    double longitud_circuferencia = Calculos.calcular_longitud_circuferencia(radio);
+    double area_circulo = Calculos.calcular_area_circulo(radio);
+
+
+    System.out.println("Longitud circuferencia "+ longitud_circuferencia);
+    System.out.println("Area circulo "+ area_circulo);
+    }
+    
+}
